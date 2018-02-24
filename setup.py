47f31710a4908bea192c0e16c17a7af99d766cd3
Brent Allen <carvetighter@gmail.com>
2018-02-24 11:54:26 -0600

    updated base setup.py

--- setup.py
+++ setup.py
@@ -8,18 +8,18 @@ here = path.abspath(path.dirname(__file__))
 #     long_description = f.read()
 
 setup(
-    name='TimerWrapper',
+    name='SqlMethods',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1',
+    version='0.0.0',
 
-    description='Timer for long time periods',
+    description='Light weight wrapper for pymssql',
     #long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/carvetighter/Timer',
+    url='https://github.com/carvetighter/SqlMethods',
 
     # Author details
     author='Brent Allen',
