cca3a4af33791ff7309f0b7054023729d45def92
Brent Allen <carvetighter@gmail.com>
2018-05-24 21:20:00 -0500

    updated inital bi_00 w/ pylint input

--- bulk_insert/bi_00.py
+++ bulk_insert/bi_00.py
@@ -1,15 +1,18 @@
+'''
+file to test bulk insert
+'''
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 #
 # File / Package Import
 #
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#        
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 
-from SqlMethods import SqlMethods
 import sys
 import os
 import pandas
+from SqlMethods import SqlMethods
 
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
@@ -22,49 +25,49 @@ import pandas
 def def_Methods(list_cluster_results, array_sparse_matrix):
     '''
     below is an example of a good method comment
-    
+
     ------------------------------------------------------------------------------------------------------------------------------
-    
+
     this method implements the evauluation criterea for the clusters of each clutering algorithms
     criterea:
            - 1/2 of the clusters for each result need to be:
-               - the average silhouette score of the cluster needs to be higher then the silhouette score of 
+               - the average silhouette score of the cluster needs to be higher then the silhouette score of
                  all the clusters combined
-               - the standard deviation of the clusters need to be lower than the standard deviation of all the 
+               - the standard deviation of the clusters need to be lower than the standard deviation of all the
                  clusters combined
            - silhouette value for the dataset must be greater than 0.5
-    
+
     Requirements:
     package time
     package numpy
     package statistics
     package sklearn.metrics
-    
+
     Inputs:
     list_cluster_results
     Type: list
     Desc: the list of parameters for the clustering object
-    list[x][0] -> type: array; of cluster results by sample in the order of the sample row passed as 
+    list[x][0] -> type: array; of cluster results by sample in the order of the sample row passed as
                         indicated by the sparse or dense array
     list[x][1] -> type: string; the cluster ID with the parameters
-    
+
     array_sparse_matrix
     Type: numpy array
     Desc: a sparse matrix of the samples used for clustering
-        
+
     Important Info:
     None
-    
+
     Return:
     object
     Type: list
     Desc: this of the clusters that meet the evaluation criterea
-    list[x][0] -> type: array; of cluster results by sample in the order of the sample row passed as 
+    list[x][0] -> type: array; of cluster results by sample in the order of the sample row passed as
                     indicated by the sparse or dense array
     list[x][1] -> type: string; the cluster ID with the parameters
     list[x][2] -> type: float; silhouette average value for the entire set of data
     list[x][3] -> type: array; 1 dimensional array of silhouette values for each data sample
-    list[x][4] -> type: list; list of lists, the cluster and the average silhoutte value for each cluster, the 
+    list[x][4] -> type: list; list of lists, the cluster and the average silhoutte value for each cluster, the
                         orders is sorted highest to lowest silhoutte value
                         list[x][4][x][0] -> int; cluster label
                         list[x][4][x][1] -> float; cluster silhoutte value
@@ -95,7 +98,7 @@ def def_Methods(list_cluster_results, array_sparse_matrix):
     # Start
     #
     #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#                
+    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 
     #--------------------------------------------------------------------------#
     # sub-section comment
@@ -107,7 +110,7 @@ def def_Methods(list_cluster_results, array_sparse_matrix):
     # sectional comment
     #
     #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#                
+    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 
     #--------------------------------------------------------------------------#
     # variable / object cleanup
@@ -131,21 +134,20 @@ def def_Methods(list_cluster_results, array_sparse_matrix):
 def main(list_args = []):
     '''
     this is the main method tests the bulk insert and single insert for the SqlInsertIntoTable method
-     
+
     Requirements:
     None
-    
+
     Inputs:
     list_args
     Type: list
     Desc: arguements for login to the sql server
     list_args[0] -> type: string; user name
     list_args[1] -> type: string; password
-        
-      
+
     Important Info:
     None
-    
+
     Return:
     None
     Type: None
@@ -178,7 +180,7 @@ def main(list_args = []):
     string_path = str(os.sep).join(list_path)
     string_path = os.path.join(string_path, 'files_to_insert')
     del list_path
-    
+
     #--------------------------------------------------------------------------#
     # db connections
     #--------------------------------------------------------------------------#
@@ -192,8 +194,8 @@ def main(list_args = []):
     # Start
     #
     #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#                
-    
+    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
     #--------------------------------------------------------------------------#
     # bulk insert
     #--------------------------------------------------------------------------#
@@ -207,10 +209,10 @@ def main(list_args = []):
     # sectional comment
     #
     #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
-    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#                
+    #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 
     #--------------------------------------------------------------------------#
     # variable / object cleanup
     #--------------------------------------------------------------------------#
-    
+
     sql_local.close()
